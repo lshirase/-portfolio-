ad159daa6919247944408957db736388eac6ee0d
lshirase <lshirase@umich.edu>
2023-07-06 12:00:08 -0700

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -15,7 +15,10 @@ export default function Document() {
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         />
       </Head>
-      <body className="bg-zinc-5 flex h-full flex-col">
+      <body
+        className="bg-zinc-5 flex h-full flex-col md:h-screen
+      "
+      >
         <Main />
         <NextScript />
       </body>
