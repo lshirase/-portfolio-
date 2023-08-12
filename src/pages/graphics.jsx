1065105a41b8575c302bbb693cf496dfe9a53c3c
lshirase <lshirase@umich.edu>
2023-08-11 19:03:11 -0700

    little fixes

--- src/pages/graphics.jsx
+++ src/pages/graphics.jsx
@@ -43,10 +43,10 @@ export default function Graphics() {
           content="Len Shirase, I'm a designer and developer based in Los Angeles, California."
         />
       </Head>
-      <div className="flex h-screen w-screen flex-col overflow-y-hidden bg-white text-black md:justify-between">
+      <div className="flex h-screen max-h-screen w-screen flex-col overflow-y-hidden bg-white text-black md:justify-between">
         <Header bgWhite />
         {/* <div className="flex w-screen flex-col px-6 pt-4 lg:px-12 lg:pt-0"> */}
-        <div className="w-full flex-initial justify-end overflow-y-hidden px-6  pt-4 md:pb-6 md:pl-6 md:pt-0  lg:pb-12 lg:pl-12 lg:pr-0">
+        <div className="w-full flex-initial justify-end px-6  pt-4 md:pb-6 md:pl-6 md:pt-0  lg:pb-12 lg:pl-12 lg:pr-0">
           <HorizontalScroll />
         </div>
       </div>
